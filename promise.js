d05292d69df5093b4440736d598407cbe94399bf
Jesus Garcia <heysusgarcia@gmail.com>
2015-05-01 14:03:05 -0700

    basic myPromise skeleton

---
+++ promise.js
@@ -0,0 +1,15 @@
+var MyPromise = function(executor) {
+  executor(resolve, reject);
+
+  function resolve(arg) {
+
+  };
+
+  var reject = function(arg) {
+
+  };
+
+  this.then = function(callback) {
+
+  };
+}
