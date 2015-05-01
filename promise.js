b635001a1ef16afc6269af94dc836b6e94eef973
Jesus Garcia <heysusgarcia@gmail.com>
2015-05-01 14:40:45 -0700

    added reject function

--- promise.js
+++ promise.js
@@ -1,9 +1,7 @@
 var MyPromise = function(executor) {
-  executor(resolve, reject);
-
   var deferreds = [];
 
-  function resolve(arg) {
+  var resolve = function(arg) {
     for (var i = 0; i < deferreds.length; i++) {
       var callbackResult = deferreds[i].callback(arg);
       deferreds[i].resolve(callbackResult);
@@ -11,31 +9,38 @@ var MyPromise = function(executor) {
   };
 
   var reject = function(arg) {
-
+    for (var i = 0; i < deferreds.length; i++) {
+      var callbackResult = deferreds[i].callback(arg);
+      deferreds[i].reject(callbackResult);
+    };
   };
 
   this.then = function(callback) {
     var newPromise = new MyPromise(function(resolve, reject) {
+      debugger
       deferreds.push({resolve: resolve, reject: reject, callback: callback});
     });
     return newPromise;
   };
+
+  executor(resolve, reject);
 }
 
 function fiveMachine(){
   return new MyPromise(function(resolve, reject){
     setTimeout(function(){
-      resolve(5);
+      //resolve(5);
+      reject("I don't know, you probably spelled initialize wrong");
     }, 1000);
   });
+}
+
+function alerter(message){
+  alert(message);
 }
 
 function logCallback(value){
   console.log('got ' + value);
 }
 
-function add37(val){
-  return val + 37;
-};
-
-fiveMachine().then(add37).then(logCallback);
+fiveMachine().then(logCallback, alerter);
